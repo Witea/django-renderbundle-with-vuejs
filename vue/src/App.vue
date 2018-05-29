ba499ccce9d8acc954a4efa99edaf29b7afed00b
William Kuo <William_kuo@partech.com>
2018-05-28 22:30:39 -0400

    initial commit
    working render bundle with entry points to vuejs components

---
+++ vue/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-app>
+    <TestComp></TestComp>
+  </v-app>
+</template>
+
+<script>
+import TestComp from "./TestComp.vue";
+export default {
+  components: {
+    TestComp
+  }
+};
+</script>
---
+++ vue/src/TestComp.vue
@@ -0,0 +1,9 @@
+<template>
+  <div v-if="this.$vuetify.breakpoint.smAndDown">
+    hello
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
